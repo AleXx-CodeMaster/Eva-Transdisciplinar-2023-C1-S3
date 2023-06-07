26c28d4b566fde4d976d0d405677c8fa2a3b0d88
SebastianSalamanc4 <135070374+SebastianSalamanc4@users.noreply.github.com>
2023-06-07 14:58:56 -0400

    Update BaseMRUA.py

--- BaseMRUA.py
+++ BaseMRUA.py
@@ -5,15 +5,17 @@ v0 = 0 #velocidad inicial
 a = 2 # aceleraciont 
 t=0 #tiempo
 
-#Iniciar pygame
-pygame.init()
-pygame.display.set_caption("movimiento rectilineo uniforme acelerado") 
-#display 
-width= 960
-height=640
-pantalla= pygame.display.set_mode((width,height))
-time= pygame.time.clock()
 
-#cargar imagen
-car_imagene= pygame.display.set_mode
+pygame.init() #incia pygame  
+pygame.display.set_caption("movimiento rectilineo uniforme acelerado")#nombre de la pestalña
+#dimensiones de la pantalla
+width= 960 #ancho
+height=640 #alto
+pantalla= pygame.display.set_mode((width,height)) #dimenciones de la pantalla
+time= pygame.time.clock() #fotogramas
+#imagen auto
+car_imagene= pygame.image.load("carpaint.png")# carga de foto de la nae
+car_rect=car_image.get_rect() #obtiene el fondo de la imgagne 
+car_rect.center= (width//2, heiht//2)
+
 
