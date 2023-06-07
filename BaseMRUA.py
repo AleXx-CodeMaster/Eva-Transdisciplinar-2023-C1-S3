116379f6876b7d0ed3d6a7053d68f5e2ba1e78c7
SebastianSalamanc4 <135070374+SebastianSalamanc4@users.noreply.github.com>
2023-06-07 15:23:03 -0400

    Update BaseMRUA.py
    El primer avance de este código se centra en el movimiento del automóvil dentro de un entorno visual implementado con la biblioteca pygame. El automóvil se representa como una imagen y se desplaza a lo largo de la pantalla. La pantalla tiene un fondo blanco que se actualiza en cada iteración del bucle principal del juego.

--- BaseMRUA.py
+++ BaseMRUA.py
@@ -1,4 +1,4 @@
-#base del movimento(auto)
+#BASE MOVIMIENTO AUTO MOVIMIENTO RECTILINEO ACELERADO
 import pygame
 #definicion de variables
 v0 = 0 #velocidad inicial 
@@ -17,5 +17,20 @@ time= pygame.time.clock() #fotogramas
 car_imagene= pygame.image.load("carpaint.png")# carga de foto de la nae
 car_rect=car_image.get_rect() #obtiene el fondo de la imgagne 
 car_rect.center= (width//2, heiht//2)
+while true:
+  for envent in pygame.event.get():  #itera en los eventos
+    if event.type==pygame.QUIT:
+      PYGAME.quit()
+      
+ #movimiento auto 
+    x = v0 * t + 0.5 * a * t ** 2 #movimeinto 
+  car_rect.centerx=int(x)
+  t+=1
+  #dibujar imagen auto
+    pantalla.fill((255, 255, 255))#relllenar color blanco el fondo
+    pantalla.blit(car_image, car_rect)
+    pygame.display.flip()#hacer los elementos visibles en la pantalla
+    time.tick(60)  # 60 fotogramas
+
 
 
