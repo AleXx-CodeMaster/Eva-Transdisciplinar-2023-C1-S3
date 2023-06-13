6f97254229120a9c66e2cbbf7f4d5637a72324a5
Claudio204g <135070524+Claudio204g@users.noreply.github.com>
2023-06-13 19:03:51 -0400

    formulas a ocupar
    formulas que se utilizan para para todo el MRUA

---
+++ formulas.py
@@ -0,0 +1,26 @@
+X = float(input("ingrese distancia a recorrer"))    #distancia
+
+Vf = float(input("ingrese la velocidad final"))     #velocidad final
+
+Vo = float(input("ingrese velocidad inicial"))      #velocidad inicial
+
+t = float(input("ingrese tiempo en horas"))         #tiempo
+
+a = float(input("ingrese aceleracion en formato Km por Hora"))  #aceleracion
+
+V = float(input("ingrese velocidad")) #velocidad es el cuanto cambia en cierto tiempo
+
+Vf_f = Vo + a * t   #formula velocidad final osea Vf
+
+X_i = ((Vf + Vo)/2) * t  #formula distancia osea X
+
+a_f = V/t #formula aceleracion osea a 
+
+V_f = Vf - Vo #formula velocidad osea V
+
+X_sVF = Vo * t + ((a*t)**2)/2  #distancia sin velocidad final Vf
+
+t_F = (Vf - Vo) / a  #formula tiempo
+
+Vf_2 = 2 * (a * X) + Vo **2  #formula Vf al cuadrado
+
